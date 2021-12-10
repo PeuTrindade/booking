ac60a0c4f7ec3ce08fc7d4b8bba86b290a7363fc
Malsa APIs <devpedrotrindade@gmail.com>
2021-12-10 17:58:43 -0300

    first commit

---
+++ protected/views/layouts/main.php
@@ -0,0 +1,25 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="language" content="pt-br">
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+<body>
+	<header class="headerContainer">
+		<nav class="sidebar">
+			<?php if(isset(Yii::app()->user->id)){ $this->widget('zii.widgets.CMenu',array(
+				'items'=>array(
+					array('label'=>'Home', 'url'=>array('/site/index')),
+					array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+					array('label'=>'Contact', 'url'=>array('/site/contact')),
+					array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				),
+			)); }?>
+		</nav>
+	</header>
+	<?php echo $content; ?>
+</body>
+</html>
---
+++ protected/views/site/login.php
@@ -0,0 +1,42 @@
+<?php
+/* @var $this SiteController */
+/* @var $model LoginForm */
+/* @var $form CActiveForm  */
+
+Yii::app()->name = 'Booking';
+$this->pageTitle = Yii::app()->name . ' - Login';
+?>
+
+<style>
+	<?php include 'css/login/login.css'; ?>
+</style>
+
+<section class="loginContainer">
+	<div class="loginInformation">
+		<h1>Faça o seu login</h1>
+		<div class="form">
+			<?php $form=$this->beginWidget('CActiveForm', array(
+				'id'=>'login-form',
+				'enableClientValidation'=>true,
+				'clientOptions'=>array(
+					'validateOnSubmit'=>true,
+				),
+			)); ?>
+			<div class="row">
+				<?php echo $form->labelEx($model,'Usuario'); ?>
+				<?php echo $form->textField($model,'username'); ?>
+				<?php echo $form->error($model,'username'); ?>
+			</div>
+			<div class="row">
+				<?php echo $form->labelEx($model,'Senha'); ?>
+				<?php echo $form->passwordField($model,'password'); ?>
+				<?php echo $form->error($model,'password'); ?>
+			</div>
+			<div class="row buttons">
+				<?php echo CHtml::submitButton('Login'); ?>
+			</div>
+			<?php $this->endWidget(); ?>
+		</div>
+	</div>
+	<img src="images/loginImage.jpg" alt="loginImage"/>
+</section>
